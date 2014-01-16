7fc72745b6ec352975cb8cce844f72b5978f9b22
Stefan heimes <stefan_heimes@hotmail.com>
2014-01-16 16:59:48 +0100

    Add a missing "k" in the word "LooUp". See #1

--- src/system/modules/metamodelsfilter_perimetersearch/languages/en/tl_metamodel_filtersetting.php
+++ src/system/modules/metamodelsfilter_perimetersearch/languages/en/tl_metamodel_filtersetting.php
@@ -31,7 +31,7 @@ $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['second_attr_id']  = array('At
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode']       = array('Rangemode', 'Here you can choose how the range will be displayed.');
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_preset']    = array('Range preset', 'Here you can add a preset range.');
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_selection'] = array('Range selection', 'Here you can add values.');
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']   = array('LooUp Services', 'Here you can choose a look up service for resolving adress data.');
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']   = array('LookUp Services', 'Here you can choose a look up service for resolving adress data.');
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_label']     = array('Range Label', 'Show range label instead of attribute name.');
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_template']  = array('Range Template', 'Sub template for this range filter element. Standard: form widget.');
 
