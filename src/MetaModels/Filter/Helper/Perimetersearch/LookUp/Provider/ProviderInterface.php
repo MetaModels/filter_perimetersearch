33bfa93da830e071e7fe5c838009a62bc183457b
Stefan Heimes <stefan_heimes@hotmail.com>
2015-03-05 13:48:45 +0100

    Improve the interface.
    - Add some default functions.
    - Change the interface to a abstract class.

--- src/MetaModels/Filter/Helper/Perimetersearch/LookUp/Provider/ProviderInterface.php
+++ src/MetaModels/Filter/Helper/Perimetersearch/LookUp/Provider/ProviderInterface.php
@@ -29,8 +29,86 @@ use MetaModels\Filter\Helper\Perimetersearch\LookUp\Container;
  * @subpackage    PerimeterSearch
  * @author        Stefan Heimes <stefan_heimes@hotmail.com>
  */
-interface ProviderInterface
+abstract class ProviderInterface
 {
+
+    /**
+     * Get a list with all countries.
+     *
+     * @return array
+     */
+    public function getCountries()
+    {
+        return \Controller::getCountries();
+    }
+
+    /**
+     * Search the full name of a country.
+     *
+     * @param string $strShort The short tag for the country.
+     *
+     * @return null|string Null on error or the full name as string.
+     */
+    public function getFullCountryName($strShort)
+    {
+        $arrCountries = $this->getCountries();
+        if (array_key_exists($strShort, $arrCountries)) {
+            return $arrCountries[$strShort];
+        }
+
+        return null;
+    }
+
+    /**
+     * Build the query string.
+     *
+     * @param string $street
+     *
+     * @param string $postal
+     *
+     * @param string $city
+     *
+     * @param string $country
+     *
+     * @param string $fullAddress
+     *
+     * @return string
+     */
+    public function getQueryString(
+        $street = null,
+        $postal = null,
+        $city = null,
+        $country = null,
+        $fullAddress = null
+    ) {
+        // If we have a full address use it.
+        if ($fullAddress) {
+            // If there is a country try to use the long form.
+            if ($country !== null && ($fullCountryName = $this->getFullCountryName($country)) !== null) {
+                return sprintf('%s, %s', $fullAddress, $fullCountryName);
+            } elseif ($country !== null) {
+                // If there is no long form use it as is it.
+                return sprintf('%s, %s', $fullAddress, $country);
+            } else {
+                // Or just the full one.
+                return $fullAddress;
+            }
+        } else {
+            // Try to solve the country.
+            if ($country !== null && ($fullCountryName = $this->getFullCountryName($country)) !== null) {
+                $country = $fullCountryName;
+            }
+
+            return sprintf(
+                "%s, %s %s, %s"
+                , $street
+                , $postal
+                , $city
+                , $country
+            );
+        }
+    }
+
     /**
      * Find coordinates for given adress
      *
@@ -46,6 +124,12 @@ interface ProviderInterface
      *
      * @return Container
      */
-    public function getCoordinates($street = null, $postal = null, $city = null, $country = null, $fullAddress = null);
+    abstract public function getCoordinates(
+        $street = null,
+        $postal = null,
+        $city = null,
+        $country = null,
+        $fullAddress = null
+    );
 
 }
