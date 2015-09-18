5cbc0fe709e29389b3087ed608a3ba43cc64601a
Mini Model <minimodel@metamodel.me>
2015-09-18 10:34:44 +0200

    Updated translations from transifex.

---
+++ contao/languages/de/tl_metamodel_filtersetting.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T08:03:46+02:00
+ */
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_get']['0']                                   = 'GET-Parameter für die Sprache';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_get']['1']                                   = 'Hier können Sie einen GET-Parameter vorgeben.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_preset']['0']                                = 'Ländervorgabe';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_preset']['1']                                = 'Hier können Sie eine Vorgabe für eine Sprache hinzufügen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode']['0']                                   = 'Ländermodus';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode']['1']                                   = 'Hier können Sie die verwendete Sprache wählen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode_options']['get']                         = 'GET-Parameter verwenden';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode_options']['none']                        = 'Nichts';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode_options']['preset']                      = 'Voreinstellung aus Systemeinstellungen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode']['0']                                      = 'Datenmodus';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode']['1']                                      = 'Hier können Sie auswählen, ob ein oder zwei Attribute für die Übergabe der Geokoordinaten genutzt werden sollen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode_options']['multi']                          = 'Multimodus - mehrere Attribute';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode_options']['single']                         = 'Einzelmodus - ein Attribut';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fefilter_legend']                                    = 'Frontendfilter-Einstellungen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['first_attr_id']['0']                                 = 'Attribut - geografische Breite (Lat)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['first_attr_id']['1']                                 = 'Bitte wählen Sie das Attribut für den geografischen Breitenwert aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['geolocation_legend']                                 = 'Geolocation-Einstellungen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']['0']                                 = 'LookUp Services';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']['1']                                 = 'Hier können Sie auswählen, welcher Dienst benutzt wird um Adressdaten aufzulösen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['perimetersearch']['PerimetersearchLookUpGoogleMaps'] = 'GoogleMaps Lookup';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['perimetersearch']['google_maps']                     = 'GoogleMaps Lookup';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['perimetersearch']['open_street_maps']                = 'OpenStreetMap Lookup';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_label']['0']                                   = 'Bereichslabel';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_label']['1']                                   = 'Das Bereichslabel anstelle des Attributnamens anzeigen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_legend']                                       = 'Bereichseinstellungen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_preset']['0']                                  = 'Bereichs-Voreinstellung';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_preset']['1']                                  = 'Hier können Sie Werte für die Größe des Suchbereiches vorbelegen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_selection']['0']                               = 'Bereichsauswahl';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_selection']['1']                               = 'Hier können Sie Werte direkt eingeben.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_template']['0']                                = 'Bereichstemplate';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_template']['1']                                = 'Subtemplate für dieses Bereichsfilter-Element. Die Standardvorgabe ist: Formwidget.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode']['0']                                     = 'Bereichsmodus';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode']['1']                                     = 'Hier können Sie auswählen, wie der Bereich angezeigt werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode_options']['free']                          = 'Freie Eingabe';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode_options']['preset']                        = 'Voreinstellung aus Systemeinstellungen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode_options']['selection']                     = 'Auswahlmodus';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['second_attr_id']['0']                                = 'Attribut - geografische Länge (Lng)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['second_attr_id']['1']                                = 'Bitte wählen Sie das Attribut für den geografischen Längenwert aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['single_attr_id']['0']                                = 'Attribut';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['single_attr_id']['1']                                = 'Wählen Sie das Attribut aus in dem die Werte für geografische Länge und Breite gespeichert sind.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['perimetersearch']                       = 'Umkreissuche';
+
