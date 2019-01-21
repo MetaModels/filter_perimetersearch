b949ed2eeb3c83bd6aafae581ee72f21de6761e6
Mini Model <minimodel@metamodel.me>
2019-01-21 23:09:38 +0100

    Updated translations from transifex.

--- contao/languages/ru/explain.php
+++ contao/languages/ru/explain.php
@@ -1,16 +1,27 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/filter_perimetersearch.
+ *
+ * (c) 2012-2019 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * Last-updated: 2019-01-21T23:09:35+01:00
  *
- * @link https://www.transifex.com/signup/
- * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
- *
- * last-updated: 2018-04-25T09:30:48+02:00
+ * @copyright 2012-2019 The MetaModels team.
+ * @license   https://github.com/MetaModels/filter_perimetersearch/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
 
 $GLOBALS['TL_LANG']['XPL']['filter_lookupservice']['0']['0'] = 'Поиск на картах Google ';
+$GLOBALS['TL_LANG']['XPL']['filter_lookupservice']['0']['1'] = 'Если вы используете службу поиска Google Maps, добавьте токен API для обеспечения полного использования.<br>
+    <strong><a href="https://developers.google.com/maps/documentation/javascript/usage">Google Maps API</a></strong>';
 
--- contao/languages/ru/tl_metamodel_filtersetting.php
+++ contao/languages/ru/tl_metamodel_filtersetting.php
@@ -1,33 +1,61 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/filter_perimetersearch.
+ *
+ * (c) 2012-2019 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ * Last-updated: 2019-01-21T23:09:38+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2018-04-25T09:30:51+02:00
+ * @copyright 2012-2019 The MetaModels team.
+ * @license   https://github.com/MetaModels/filter_perimetersearch/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_get']['0']                    = 'GET-Параметр для страны';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_get']['1']                    = 'Здесь вы можете добавить название GET-Параметра для поиска в стране.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_preset']['0']                 = 'Предустановленная страна';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_preset']['1']                 = 'Вы можете предварительно задать страну.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode']['0']                    = 'Режим страны';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode']['1']                    = 'Вы можете выбрать, какая будет использована страна.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode_options']['get']          = 'Использовать параметр GET';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode_options']['none']         = 'Нет';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['countrymode_options']['preset']       = 'Предварительно заданный системой';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode']['0']                       = 'Режим данных';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode']['1']                       = 'Вы можете выбрать, один атрибут или два атрибута.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode_options']['multi']           = 'Мульти-режим - два атрибута';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['datamode_options']['single']          = 'Одиночный режим - один атрибут';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fefilter_legend']                     = 'Настройки фильтра внешнего интерфейса';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['first_attr_id']['0']                  = 'Атрибут - Широта';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['first_attr_id']['1']                  = 'Выберите атрибут значения Широта.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['geolocation_legend']                  = 'Настройки геолокации';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']['0']                  = 'Услуги LookUp';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']['1']                  = 'Здесь вы можете выбрать службу поиска для разрешения адресных данных.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']['api_token']['0']     = 'API токен';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lookupservice']['api_token']['1']     = 'Здесь вы можете добавить токен API.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['perimetersearch']['google_maps']      = 'Поиск на картах Google ';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['perimetersearch']['open_street_maps'] = 'OpenStreetMap';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_label']['0']                    = 'Значок диапазона';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_label']['1']                    = 'Показать метки диапазона вместо названия атрибута.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_legend']                        = 'Настройки диапазона';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_placeholder']['0']              = 'Заполнитель диапазона';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_placeholder']['1']              = 'Показывать этот текст, если оно пустое (требуется HTML5).';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_preset']['0']                   = 'Предварительно заданный диапазон';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_preset']['1']                   = 'Вы можете добавить предварительно заданный диапазон.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_selection']['0']                = 'Выбор диапазона';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_selection']['1']                = 'Вы можете добавить значения.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_template']['0']                 = 'Шаблон диапазона';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_template']['1']                 = 'Подшаблон для этого диапазона элемента фильтра. Стандарт: виджет формы.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode']['0']                      = 'Режим диапазона';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode']['1']                      = 'Вы можете выбрать, как будет показан диапазон.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode_options']['free']           = 'Режим свободного ввода';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['rangemode_options']['preset']         = 'Предварительно заданный системой';
