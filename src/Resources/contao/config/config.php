95914a8158d30575c28fe4833bc7382b2c5f2dfb
Ingolf Steinhardt <info@e-spin.de>
2021-01-06 19:21:36 +0000

    Add lookup service coordinates

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_perimetersearch.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,17 +14,19 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_perimetersearch/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
+use MetaModels\FilterPerimetersearchBundle\FilterHelper\Coordinates;
 use MetaModels\FilterPerimetersearchBundle\FilterHelper\GoogleMaps;
 use MetaModels\FilterPerimetersearchBundle\FilterHelper\OpenStreetMaps;
 
 /*
  * Plugins for geo resolving.
  */
-
+$GLOBALS['METAMODELS']['filters']['perimetersearch']['resolve_class']['coordinates']      = Coordinates::class;
 $GLOBALS['METAMODELS']['filters']['perimetersearch']['resolve_class']['google_maps']      = GoogleMaps::class;
 $GLOBALS['METAMODELS']['filters']['perimetersearch']['resolve_class']['open_street_maps'] = OpenStreetMaps::class;
