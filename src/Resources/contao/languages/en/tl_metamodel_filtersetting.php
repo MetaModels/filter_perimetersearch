272ebc5d5a1d6d7f2d36db8d69f95d08adbbfe91
Ingolf Steinhardt <info@e-spin.de>
2022-07-10 13:15:34 +0200

    Fix country legend

--- src/Resources/contao/languages/en/tl_metamodel_filtersetting.php
+++ src/Resources/contao/languages/en/tl_metamodel_filtersetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_perimetersearch.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_perimetersearch/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -26,6 +26,7 @@
  */
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['geolocation_legend'] = 'Geolocation settings';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['range_legend']       = 'Range settings';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['country_legend']     = 'Country settings';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fefilter_legend']    = 'Frontendfilter settings';
 
 /**
