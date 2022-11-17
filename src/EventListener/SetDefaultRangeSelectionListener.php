4c45f2ea950af6bd4f3435fe1bd608552efdbb85
Sven Baumann <baumann.sv@gmail.com>
2022-11-17 09:19:29 +0100

    Fix autoload validation issue

--- src/EventListener/SetDefaultRangeSelectionListener.php
+++ src/EventListener/SetDefaultRangeSelectionListener.php
@@ -12,6 +12,7 @@
  *
  * @package    MetaModels/filter_perimetersearch
  * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_perimetersearch/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
